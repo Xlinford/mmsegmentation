e5d04d902076a58a05915daac73da83e99de53b3
lfxuan <xlinford@163.com>
2021-01-22 21:29:02 +0800

    head delete

--- mmseg/models/decode_heads/sep_aspp_head.py
+++ mmseg/models/decode_heads/sep_aspp_head.py
@@ -185,10 +185,7 @@ class DepthwiseSeparableASPPHead2Inputs(ASPPHead):
     def forward(self, inputs, inputs2=None):
         """Forward function."""
         output = None
-<<<<<<< HEAD
-=======
         feat = None
->>>>>>> c2a36694f449f940e6df9172b60d269b7644c626
         x = self._transform_inputs(inputs)
 
         if inputs2 is not None:
@@ -198,12 +195,9 @@ class DepthwiseSeparableASPPHead2Inputs(ASPPHead):
             x_feat = self.mlp(x)
             x2_feat = self.mlp(x2)
             output = self.head(x2, inputs2)
-<<<<<<< HEAD
-=======
             x_feat.unsqueeze(1)
             x2_feat.unsqueeze(2)
             feat = torch.cat([x_feat, x2_feat], dim=1)
->>>>>>> c2a36694f449f940e6df9172b60d269b7644c626
 
         if output is not None:
             output1 = self.head(x, inputs)
@@ -213,13 +207,7 @@ class DepthwiseSeparableASPPHead2Inputs(ASPPHead):
         else:
             output = self.head(x, inputs)
 
-<<<<<<< HEAD
-        
-        return output
-=======
-        if feat != None:
+        if feat is not None:
             return output, feat
         else:
             return output
-
->>>>>>> c2a36694f449f940e6df9172b60d269b7644c626
