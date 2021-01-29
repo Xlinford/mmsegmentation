8593714bc934cae332d9b1939be5556c4eee6796
Linford@Xiaoxin <xlinford@163.com>
2021-01-29 21:14:56 +0800

    debug

--- configs/_base_/models/deeplabv3plus_r50-d8-dcac.py
+++ configs/_base_/models/deeplabv3plus_r50-d8-dcac.py
@@ -27,7 +27,7 @@ model = dict(
         norm_cfg=norm_cfg,
         align_corners=False,
         loss_decode=dict(
-            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+            type='PixelwiseContrastiveLoss', use_sigmoid=False, loss_weight=1.0)),
     auxiliary_head=dict(
         type='FCNHead',
         in_channels=1024,
