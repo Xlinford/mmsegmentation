c5ba7e0c0e21ef2e2ad30291d01b891e0e3d71ee
lfxuan <xlinford@163.com>
2021-03-03 15:33:17 +0800

    ipdb

--- configs/_base_/schedules/schedule_5epoch.py
+++ configs/_base_/schedules/schedule_5epoch.py
@@ -5,6 +5,5 @@ optimizer_config = dict()
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
 # runtime settings
 runner = dict(type='EpochBasedRunner', max_epochs=5)
-runner = dict(type='EpochBasedRunner', max_epochs=5)
 checkpoint_config = dict(by_epoch=True, interval=5)
 evaluation = dict(interval=2000, metric='mIoU')
