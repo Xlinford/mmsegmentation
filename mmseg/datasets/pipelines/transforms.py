3cd40af78311c165ea6e27a7a7eb015708491cb8
lfxuan <xlinford@163.com>
2021-03-03 17:00:20 +0800

    ipdb

--- mmseg/datasets/pipelines/transforms.py
+++ mmseg/datasets/pipelines/transforms.py
@@ -580,7 +580,7 @@ class RandomCrop(object):
         img_shape = img.shape
         results['img'] = img
         results['img_shape'] = img_shape
-        if img.shape[:2] is not (320, 320, 3):
+        if img.shape != (320, 320, 3):
             ipdb.set_trace()
         # crop semantic seg
         for key in results.get('seg_fields', []):
@@ -720,7 +720,7 @@ class RGB2Gray(object):
 
         results['img'] = img
         results['img_shape'] = img.shape
-        if img.shape[:2] is not (320, 320, 3):
+        if img.shape is not (320, 320, 3):
             ipdb.set_trace()
         return results
 
@@ -1072,7 +1072,7 @@ class RandomMIOUCrop(object):
         corner_position1, corner_position2 = self.get_crop_corner(cover_crop_box)
 
         results['img_shape'] = img_shape
-        if img.shape[:2] is not (320, 320, 3):
+        if img.shape is not (320, 320, 3):
             ipdb.set_trace()
         results['cover_crop_box'] = [corner_position1, corner_position2]
 
