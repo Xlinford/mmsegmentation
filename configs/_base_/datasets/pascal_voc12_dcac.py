870549d7efe76b6cbb18115baff65cf601c57f96
lfxuan <xlinford@163.com>
2021-03-03 16:51:11 +0800

    ipdb

--- configs/_base_/datasets/pascal_voc12_dcac.py
+++ configs/_base_/datasets/pascal_voc12_dcac.py
@@ -45,7 +45,7 @@ test_pipeline = [
 ]
 data = dict(
     samples_per_gpu=2,
-    workers_per_gpu=2,
+    workers_per_gpu=0,
     train=dict(
         type=dataset_type,
         data_root=data_root,
