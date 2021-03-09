a417f1bbd487baf915f89e32671fe344d2f70f75
lfxuan <xlinford@163.com>
2021-03-09 13:15:58 +0800

    temp

--- mmseg/apis/train.py
+++ mmseg/apis/train.py
@@ -117,7 +117,7 @@ def train_segmentor(model,
     if 'semi_train' in cfg.data.keys():
         epoch_max_iters = len(data_loaders[1])
         # zipdata = ZipDataset(data_loaders)
-        data_loaders = iter(zip(cycle(data_loaders[0]), data_loaders[1]))
+        data_loaders = [iter(zip(cycle(data_loaders[0]), data_loaders[1]))]
         # data_loaders = [zipdata.zip_dataset_length(data_loaders)]
         runner.run(data_loaders, cfg.workflow, epoch_max_iters=epoch_max_iters)
     else:
