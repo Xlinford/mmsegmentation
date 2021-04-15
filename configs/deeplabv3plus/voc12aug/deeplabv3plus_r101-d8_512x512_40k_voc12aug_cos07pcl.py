0c81cf8d32f78b6f14baba2397c789448d1f3d56
xlinford-240 <xlinford@gmail.com>
2021-04-15 08:56:49 +0800

    lw025->02

--- configs/deeplabv3plus/voc12aug/deeplabv3plus_r101-d8_512x512_40k_voc12aug_cos07pcl.py
+++ configs/deeplabv3plus/voc12aug/deeplabv3plus_r101-d8_512x512_40k_voc12aug_cos07pcl.py
@@ -3,7 +3,7 @@ model = dict(pretrained='open-mmlab://resnet101_v1c', backbone=dict(depth=101),
              decode_head=dict(num_classes=21,
                               loss_decode=dict(type='KLPatchContrastiveLoss',
                                                use_sigmoid=False,
-                                               loss_weight=0.25,
+                                               loss_weight=0.2,
                                                cal_function='COS07',
                                                cal_gate=99)))
-data = dict(samples_per_gpu=3, workers_per_gpu=4)
+data = dict(samples_per_gpu=2, workers_per_gpu=2)
