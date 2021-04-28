8b444de5e6db2af2538a73a93ac75204f5c3bb2f
xlinford <xlinford@gmail.com>
2021-04-28 19:28:48 +0800

    config

--- configs/deeplabv3plus/ade20k/deeplabv3plus_r101-d8_512x512_160k_ade20k_cos07pcl.py
+++ configs/deeplabv3plus/ade20k/deeplabv3plus_r101-d8_512x512_160k_ade20k_cos07pcl.py
@@ -2,8 +2,12 @@ _base_ = '../deeplabv3plus_r50-d8_512x512_160k_ade20k.py'
 model = dict(pretrained='open-mmlab://resnet101_v1c', backbone=dict(depth=101),
              decode_head=dict(loss_decode=dict(type='KLPatchContrastiveLoss',
                                                use_sigmoid=False,
-                                               loss_weight=0.1,
+                                               loss_ratio=True,
+                                               loss_weight=1,
                                                cal_function='COS07',
-                                               cal_gate=99)))
-checkpoint_config = dict(by_epoch=False, interval=16000)
-evaluation = dict(interval=4000, metric='mIoU')
+                                               cal_gate=[20, 199])))
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
+lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
+log_config = dict(interval=50,
+                  hooks=[dict(type='TextLoggerHook', by_epoch=False),
+                         dict(type='TensorboardLoggerHook')])
--- configs/deeplabv3plus/citysacpes/deeplabv3plus_r101-d8_512x1024_80k_cityscapes_cos07pcl.py
+++ configs/deeplabv3plus/citysacpes/deeplabv3plus_r101-d8_512x1024_80k_cityscapes_cos07pcl.py
@@ -3,9 +3,9 @@ model = dict(pretrained='open-mmlab://resnet101_v1c', backbone=dict(depth=101),
              decode_head=dict(loss_decode=dict(type='KLPatchContrastiveLoss',
                                                use_sigmoid=False,
                                                loss_ratio=True,
-                                               loss_weight=1.1,
+                                               loss_weight=1,
                                                cal_function='COS07',
-                                               cal_gate=[49, 199])))
+                                               cal_gate=[20, 199])))
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
 runner = dict(type='IterBasedRunner', max_iters=80000)
